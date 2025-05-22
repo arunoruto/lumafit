6f3c13f09888158c2e8cd869198d4f8564bb559d
Mirza Arnaut <mirza.arnaut45@gmail.com>
2025-05-22 15:32:14 +0200

    docs: add the __version__ string to the project

--- lumafit/__init__.py
+++ lumafit/__init__.py
@@ -14,6 +14,7 @@ import numpy as np
 import numpy.typing as npt
 from numba import jit, prange
 
+__version__ = "0.2.3"
 # Define epsilon near machine precision for numerical stability
 _EPS = np.finfo(float).eps
 
